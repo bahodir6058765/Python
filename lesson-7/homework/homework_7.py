a2f6dfaadff5fdb5beb1ddecc9b45564f9491ead
bahodir6058765 <gofurovmuhammadamin10@gmail.com>
2025-09-09 09:47:26 +0500

    Create homework_7.py

---
+++ lesson-7/homework/homework_7.py
@@ -0,0 +1,36 @@
+# 1. Students fayldagi ismlarni sanash
+from collections import Counter
+# Faylni o'qish
+with open("Students.txt", "r", encoding="utf-8") as f:
+    data = f.read().split()
+chastota = Counter(data)
+# Natijani chiqarish
+for soz,soni in chastota.items():
+    print(f"'{soz}': {soni} marta")
+
+# 1. is_prime(n) funksiyasi
+# is_prime(n) funksiyasini hosil qiling (n > 0). Agar n soni tub bo'lsa True, aks holda False qiymat qaytarsin.
+def is_prime(n):
+    s = 0
+    for i in range(1,n):
+        if n%i==0:
+            s += 1
+        elif s <= 2:
+            return True
+        else:
+            return False
+is_prime(7)
+
+# 2. digit_sum(k) funksiyasi
+# digit_sum(k) funksiyasini yozing, u k sonining raqamlari yig'indisini hisoblaydi.
+def digit_sum(k):
+    return sum(int(i) for i in str(k))
+print(digit_sum(502))
+# 3. Ikki sonning darajalari
+# Berilgan N sonidan oshmaydigan barcha 2 ning darajalarini (ya'ni, 2**k shaklidagi sonlarni) chop etuvchi funksiyani yozing.
+def daraja(n):
+    i = 0
+    while 2**i <= n:
+        print(2**i)
+        i += 1
+daraja(32)
